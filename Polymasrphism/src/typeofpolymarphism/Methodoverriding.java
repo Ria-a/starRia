f96cb02a8c03452e9cf10ee80135963c2cba4d11
Rayhana Ferdous <Rayhana Ferdous@192.168.0.13>
2021-03-03 12:49:48 -0500

    prozect commit by ria

---
+++ Polymasrphism/src/typeofpolymarphism/Methodoverriding.java
@@ -0,0 +1,12 @@
+package typeofpolymarphism;
+
+public class Methodoverriding extends Methodoverloding {
+	
+public static void main(String[] args) {
+	
+ Methodoverloding k = new Methodoverloding ();
+ k.ria("ria");
+ k.ria(27);
+ k.ria(7000, "lawyer");
+}
+	}
---
+++ Polymasrphism/src/typeofpolymarphism/MethodoverridingA.java
@@ -0,0 +1,17 @@
+package typeofpolymarphism;
+
+public class MethodoverridingA {
+
+	public void ria(String name) {
+	System.out.println("my name is"+ name);
+}
+
+    public void ria(int age) {
+	System.out.println("my age is"+ age );
+  }
+
+
+    public void ria( int salary, String jobnam) {
+    	System.out.println("my salary is"+ salary +  "my job company name"+ jobnam);
+    }
+}
